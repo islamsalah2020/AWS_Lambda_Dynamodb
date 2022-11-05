80215cebb2b7db503d1e767bcc534f7e535236c9
Islam Salah <46622354+islamsalah2020@users.noreply.github.com>
2022-11-05 22:42:01 +0200

    Create PutItem.py

---
+++ PutItem.py
@@ -0,0 +1,22 @@
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+
+table = dynamodb.Table('planets')
+
+
+
+def lambda_handler(event, context):
+    table.put_item(
+    Item={'id': 'earth', 'temp': 'super cool'}
+    )
+    
+    response = {
+      'message': 'Item added'
+    }
+    
+    return {
+        'statusCode': 200,
+        'body': response
+    }
