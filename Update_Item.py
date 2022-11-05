3d33d0af4231157c8650a11a9fd1cb5efccafbbe
Islam Salah <46622354+islamsalah2020@users.noreply.github.com>
2022-11-05 23:27:18 +0200

    Create Update_Item.py

---
+++ Update_Item.py
@@ -0,0 +1,26 @@
+import json
+import boto3
+
+dynamodb = boto3.resource('dynamodb')
+
+table = dynamodb.Table('planets')
+
+
+
+def lambda_handler(event, context):
+    response = table.update_item(
+    Key={"id": "mercury"},
+    UpdateExpression="set size=:size",
+        ExpressionAttributeValues={
+            ":size": "900000000km2"
+        },
+        ReturnValues="UPDATED_NEW"
+)
+    
+    
+    print (response)
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': response
+    }
